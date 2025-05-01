12c9c066ed47b208cf36dfe97ae52d65cb8b28ae
Julian Sarmiento <julian010422@gmail.com>
2025-05-01 16:11:13 -0500

    update database

--- microservices/taskflow/app/core/config.py
+++ microservices/taskflow/app/core/config.py
@@ -1,12 +1,15 @@
 from pydantic_settings import BaseSettings
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parents[2]  # retrocede 2 niveles hasta taskflow/
+ENV_PATH = BASE_DIR / ".env"
 class Settings(BaseSettings):
-    database_url: str = "sqlite:///./test.db"
-    secret_key: str = "supersecretkey"
-    algorithm: str = "HS256"
-    access_token_expire_minutes: int = 30
+    database_url: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
 
     class Config:
-        env_file = ".env"
+        env_file = ENV_PATH
 
 settings = Settings()
--- microservices/taskflow/scripts/init_db.py
+++ microservices/taskflow/scripts/init_db.py
@@ -1,5 +1,5 @@
-from microservices.taskflow.app.db.session import engine
-from microservices.taskflow.app.db.base import Base
+from app.db.session import engine
+from app.db.base import Base
 
 def init_db():
     Base.metadata.create_all(bind=engine)
